8b5b4187d0dca2962274fdfdafc0445808202756
anthonynarine <anarine83@gmail.com>
2024-07-13 10:23:52 -0400

    Main component added to project blank shells of all 3 drawers are not completed and can be reused

---
+++ frontend/chat/src/pages/scaffold/main/Main.jsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import React from "react";
+
+/**
+ * A reusable main container component.
+ * This component is typically used as the main content area in wider views.
+ *
+ * @param {object} props - The properties of the Main component.
+ * @param {React.ReactNode} props.children - Elements to be rendered inside the Main component.
+ * @returns {JSX.Element} The JSX representation of the Main component.
+ */
+const Main = function ({ children }) {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        mt: `${theme.primaryAppBar.height}px`,
+        height: `calc(100vh - ${theme.primaryAppBar.height}px)`,
+        overflow: "hidden",
+      }}
+    >
+      {[...Array(40)].map((_, i) => (
+        <Typography key={i} paragraph>
+          {i + 1}
+        </Typography>
+      ))}
+      {children}
+    </Box>
+  );
+};
+
+export default Main;
